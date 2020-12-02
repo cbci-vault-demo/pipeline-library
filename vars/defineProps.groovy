59735b19c802ec8473be071b3605eaf7a44e91f6
Beedemo Developer <36600505+beedemo-dev@users.noreply.github.com>
2020-12-02 15:37:46 -0500

    Update defineProps.groovy

--- vars/defineProps.groovy
+++ vars/defineProps.groovy
@@ -1,9 +1,5 @@
 // vars/defineProps.groovy
 def call(String file) {
-  //defaults from job name for Docker image
-  imageNameTag()
-  //set SHORT_COMMIT for all defineProps calls
-  gitShortCommit()
   //use the Pipeline Utility Steps plugin readProperties step to read the .nodejs-app custom marker file 
   def props = readProperties file: file
   for ( e in props ) {
